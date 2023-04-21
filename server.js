060c72ea5e74fbb77b78e640aba10b9a5e6d05ea
Anthony <anthony@Anthonys-MacBook-Air.local>
2023-04-21 15:53:23 -0400

    setting up backend

---
+++ server.js
@@ -0,0 +1,34 @@
+require('dotenv').config();
+require('./config/database');
+
+const express = require('express');
+const path = require('path');
+const logger = require('morgan');
+const PORT = process.env.PORT || 3001;
+const app = express();
+
+app.use(express.json);
+
+app.use((req, res, next) => {
+  res.locals.data = {};
+  next();
+});
+
+app.use(logger('dev'));
+app.use(express.static(path.join(__dirname, 'build')));
+
+app.use(require('./config/checkToken'));
+app.use('/api/users', require('./routes/api/users'));
+app.use('/api/invoices', require('./routes/api/invoices'));
+
+app.get('/api/test', (req, res) => {
+  res.send('api is working');
+});
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'build', 'index.html'));
+});
+
+app.listen(PORT, () => {
+  console.log(`Connected to ${PORT}.`);
+});
